ad6d4a028f18a0885e9b94da4f2742462d68d254
Louis Ditzel <louisditzel@gmail.com>
2014-11-23 15:05:32 +0000

    Navigation & bug fixes
    Adds navigation around app (up arrows, etc)
    Fixes graphical bugs on Lollipop (elevation & cardview)

--- Bath Touch/app/src/main/java/company/businessinc/bathtouch/HomePageActivity.java
+++ Bath Touch/app/src/main/java/company/businessinc/bathtouch/HomePageActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
@@ -28,6 +29,10 @@ public class HomePageActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage_activity);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_home_screen);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
 
         mRecyclerView = (RecyclerView) findViewById(R.id.home_page_cards_recycle);
 
--- Bath Touch/app/src/main/java/company/businessinc/bathtouch/LeagueTableActivity.java
+++ Bath Touch/app/src/main/java/company/businessinc/bathtouch/LeagueTableActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -23,6 +24,11 @@ public class LeagueTableActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_league_table);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_league_screen);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
 
         mRecyclerView = (RecyclerView) findViewById(R.id.league_table_recycle);
 
