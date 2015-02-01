559a1bdf71b84dac0cf3cdb6cc637ffd7459bb33
Louis Ditzel <louisditzel@gmail.com>
2015-02-01 16:34:50 +0000

    Match Activity tabs
    Now displaying league table and dummy tabs

--- Bath Touch/app/src/main/java/company/businessinc/bathtouch/MatchActivity.java
+++ Bath Touch/app/src/main/java/company/businessinc/bathtouch/MatchActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.database.Cursor;
 import android.graphics.Color;
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -35,7 +36,8 @@ import company.businessinc.dataModels.League;
 import company.businessinc.dataModels.Match;
 
 
-public class MatchActivity extends ActionBarActivity implements ResultsListFragment.ResultsListCallbacks {
+public class MatchActivity extends ActionBarActivity implements LeagueFragment.LeagueCallbacks,
+        MatchFactsFragment.OnFragmentInteractionListener {
 
     private static final String TAG = "MatchActivty";
     private String mTeamOneName,mTeamTwoName;
@@ -130,28 +132,21 @@ public class MatchActivity extends ActionBarActivity implements ResultsListFragm
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_match, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
     @Override
-    public void onResultsItemSelected(int position) {
+    public void onLeagueItemSelected(int position) {
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
     }
 
@@ -163,9 +158,15 @@ public class MatchActivity extends ActionBarActivity implements ResultsListFragm
 
         @Override
         public Fragment getItem(int position) {
-            Log.d("Team Results", "Creating fragment");
-            ResultsListFragment frag = ResultsListFragment.newInstance(1);
-            return frag;
+            switch (position) {
+                case 1:
+                    LeagueFragment leagueFragment = LeagueFragment.newInstance(mLeagueID);
+                    return leagueFragment;
+                default:
+                    MatchFactsFragment matchFactsFragment = MatchFactsFragment.newInstance("a", "b");
+                    return matchFactsFragment;
+            }
+
         }
 
         @Override
