ffb1bb0971c2e714d0d275d0c8d04e33b16238a9
James Hughes <jh13293@my.bristol.ac.uk>
2015-02-22 18:31:45 +0000

    changed all fonts to roboto-thin

--- Bath Touch/app/src/main/java/company/businessinc/bathtouch/TeamRosterActivity.java
+++ Bath Touch/app/src/main/java/company/businessinc/bathtouch/TeamRosterActivity.java
@@ -124,13 +124,13 @@ public class TeamRosterActivity extends FragmentActivity implements ActionBar.Ta
         mMatchPlace = (TextView) findViewById(R.id.team_roster_match_place);
         mTeamTwoName = (TextView) findViewById(R.id.team_roster_team_1_name);
 
-        mGhostPlayer = (Button) findViewById(R.id.team_roster_create_ghost);
-        mGhostPlayer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startCreateGhostPlayerIntent();
-            }
-        });
+//        mGhostPlayer = (Button) findViewById(R.id.team_roster_create_ghost);
+//        mGhostPlayer.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startCreateGhostPlayerIntent();
+//            }
+//        });
 
         mLeagueName.setText("");
         SimpleDateFormat sdf = new SimpleDateFormat("EEE, dd MMM");
