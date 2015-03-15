c8d680675aafdd1ffc015ad653772c402f433c08
gp13823 <gp13823@my.bristol.ac.uk>
2015-03-15 23:48:26 +0000

    League is now expandable

--- Bath Touch/app/src/main/java/company/businessinc/bathtouch/adapters/LeagueTableAdapter.java
+++ Bath Touch/app/src/main/java/company/businessinc/bathtouch/adapters/LeagueTableAdapter.java
@@ -1,9 +1,12 @@
 package company.businessinc.bathtouch.adapters;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -38,90 +41,79 @@ public class LeagueTableAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
     private List<LeagueTeam> leagueTeams = new ArrayList<LeagueTeam>();
     private List<Team> allTeams = new ArrayList<Team>();
     private Context mContext;
-    private int previousExpandedLoc = -1;
-    private View previousExpandedView = null;
     private int mTeamId;
     private int mTeamColor;
     private int leagueID;
 
 
-    public void animateChange(View v, int pos) {
-        if(previousExpandedLoc == -1){
-
-            previousExpandedLoc = pos;
-            previousExpandedView = v;
-        }
-        //same item clicked again, close it
-        else if(previousExpandedLoc == pos){
-            previousExpandedLoc = pos;
-            previousExpandedView = v;
-        }
-        //another item clicked, close previosuly open one
-        else{
-            ExpandAnimation animation = new ExpandAnimation(previousExpandedView, 500);
-
-            notifyItemChanged(pos);
-
-            previousExpandedView.startAnimation(animation);
-
-        }
-
-        ExpandAnimation animation = new ExpandAnimation(v, 500);
-
-        notifyItemChanged(pos);
-
-        v.startAnimation(animation);
-
-        previousExpandedView = v;
-        previousExpandedLoc = pos;
-    }
-
-
     public class ViewHolderLeague extends RecyclerView.ViewHolder implements View.OnClickListener {
         public TextView mTeamName, mTeamWin, mTeamLose, mTeamForfeits, mTeamDraw, mTeamPts, mPtsFor, mPtsAgn, mCaptainName, mTeamPos;
         public ImageView mImagePosition;
-        public RelativeLayout mDisplayItem, mTextArea, mExtraText;
-
-        public boolean expanded = false;
+        public RelativeLayout mExpandable;
+        public CardView mMain;
 
         public ViewHolderLeague(View v) {
             super(v);
+            mMain = (CardView) v.findViewById(R.id.league_item_team1_container);
+            mMain.setOnClickListener(this);
             mTeamName = (TextView) v.findViewById(R.id.league_item_team_name);
-//            mTeamPos = (TextView) v.findViewById(R.id.league_item_team_position);
             mTeamWin = (TextView) v.findViewById(R.id.league_item_team_won);
             mTeamLose = (TextView) v.findViewById(R.id.league_item_team_lose);
             mTeamDraw = (TextView) v.findViewById(R.id.league_item_team_draw);
             mTeamForfeits = (TextView) v.findViewById(R.id.league_item_team_forfeit);
             mTeamPts = (TextView) v.findViewById(R.id.league_item_team_points);
-//            mPtsFor = (TextView) v.findViewById(R.id.league_item_ptsfor);
-//            mPtsAgn = (TextView) v.findViewById(R.id.league_item_ptsagn);
             mCaptainName = (TextView) v.findViewById(R.id.league_item_captain_name);
             mImagePosition = (ImageView) v.findViewById(R.id.image_view);
-//            mImageFor = (ImageView) v.findViewById(R.id.league_display_pointsfor_image);
-//            mImageAgn = (ImageView) v.findViewById(R.id.league_display_pointsagn_image);
-//            mCloseButton = (ImageView) v.findViewById(R.id.league_item_close_button);
-//            mExpandArea = (RelativeLayout) v.findViewById(R.id.llExpandArea);
-            mDisplayItem = (RelativeLayout) v.findViewById(R.id.league_display_item_container);
-            mTextArea = (RelativeLayout) v.findViewById(R.id.league_item_text_container);
-            mExtraText = (RelativeLayout) v.findViewById(R.id.league_item_extra_text_container);
+            mExpandable = (RelativeLayout) v.findViewById(R.id.league_item_extra_text_container);
             mPtsFor = (TextView) v.findViewById(R.id.league_item_pts_for);
             mPtsAgn = (TextView) v.findViewById(R.id.league_item_pts_agn);
             mTeamPos = (TextView) v.findViewById(R.id.league_item_team_position);
-            //mDisplayItem.setOnClickListener(this);
-
-//            mCloseButton.setOnClickListener(this);
-//            mTeamOverviewButton.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    Toast.makeText(mContext, "No Team Overview Fragment", Toast.LENGTH_SHORT).show();
-//                }
-//            });
 
         }
 
         @Override
         public void onClick(View v) {
-            animateChange(v, getPosition());
+            if(v.getId() == mMain.getId()){
+                ValueAnimator animator; //expand the player
+                if (mExpandable.getVisibility() == View.GONE) {
+                    mExpandable.setVisibility(View.VISIBLE); //expand the view
+                    final int widthSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+                    final int heightSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+                    mExpandable.measure(widthSpec, heightSpec);
+                    animator = ValueAnimator.ofInt(0, mExpandable.getMeasuredHeight());
+                } else {
+                    animator = ValueAnimator.ofInt(mExpandable.getHeight(), 0);
+                    animator.addListener(new Animator.AnimatorListener() { //listen to the end of animation and then get rid off the view
+                        @Override
+                        public void onAnimationStart(Animator animation) {
+                        }
+
+                        @Override
+                        public void onAnimationCancel(Animator animation) {
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animator animation) {
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animator animator) {
+                            mExpandable.setVisibility(View.GONE);
+                        }
+                    });
+                }
+                animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                        //Update Height
+                        int value = (Integer) valueAnimator.getAnimatedValue();
+                        ViewGroup.LayoutParams layoutParams = mExpandable.getLayoutParams();
+                        layoutParams.height = value;
+                        mExpandable.setLayoutParams(layoutParams);
+                    }
+                });
+                animator.start();
+            }
         }
 
 
@@ -178,14 +170,7 @@ public class LeagueTableAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
         // create a new view
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.league_item, parent, false);
-        ViewHolderLeague vh = new ViewHolderLeague(v);
-        // set the view's size, margins, paddings and layout parameters
-
-
-
-        return vh;
-
-
+        return new ViewHolderLeague(v);
     }
 
     @Override
@@ -205,9 +190,8 @@ public class LeagueTableAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
 
         LeagueTeam team = leagueTeams.get(position);
         Team fullTeam = null;
-        int circleColor = Color.GRAY;
+        int circleColor;
         int textColor = mContext.getResources().getColor(R.color.body_text_2);
-        int circleTextColor = Color.WHITE;
 
         for (Team e : allTeams) {
             if (e.getTeamID() == team.getTeamID()) {
@@ -224,14 +208,7 @@ public class LeagueTableAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
             Log.d("LEAGUETABLEDAPATER", "No team found in db for leagueTeam");
         }
         try {
-            if (mTeamId == team.getTeamID()) {
-                v.mDisplayItem.setBackgroundColor(mTeamColor);
-                textColor = Color.WHITE;
-                circleTextColor = mTeamColor;
-                circleColor = Color.WHITE;
-            } else {
-                circleColor = Color.parseColor(fullTeam.getTeamColorPrimary());
-            }
+            circleColor = Color.parseColor(fullTeam.getTeamColorPrimary());
         } catch (Exception e) {
             circleColor = Color.LTGRAY;
             Log.d("LEAGUEADAPTER", "Team colors still null in db");
@@ -253,48 +230,15 @@ public class LeagueTableAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
         v.mTeamPts.setText(team.getLeaguePoints().toString());
         v.mTeamPts.setTypeface(null, Typeface.BOLD);
         v.mTeamPts.setTextColor(textColor);
-        v.mPtsFor.setText("\u25B2  " + team.getPointsFor().toString());
-        v.mPtsAgn.setText("\u25BC  " + team.getPointsAgainst().toString());
+        v.mPtsFor.setText(team.getPointsFor().toString());
+        v.mPtsAgn.setText(team.getPointsAgainst().toString());
 
         //Set circle icons for positions, points for and against
         String leaguePosition = team.getPosition().toString();
-        TextDrawable drawable = TextDrawable.builder()
-                .beginConfig()
-                .textColor(circleTextColor)
-                .toUpperCase()
-                .endConfig()
-                .buildRound(leaguePosition, circleColor);
 
 //        v.mImagePosition.setImageDrawable(drawable);
         v.mTeamPos.setText(leaguePosition);
         v.mTeamPos.setTextColor(circleColor);
-
-//        String downArrow = "\u25BC";
-//        String upArrow = "\u25B2";
-//        drawable = TextDrawable.builder()
-//                .beginConfig()
-//                .textColor(Color.WHITE)
-//                .toUpperCase()
-//                .endConfig()
-//                .buildRound(upArrow, mContext.getResources().getColor(R.color.green));
-//        v.mImageFor.setImageDrawable(drawable);
-//
-//        drawable = TextDrawable.builder()
-//                .beginConfig()
-//                .textColor(Color.WHITE)
-//                .toUpperCase()
-//                .endConfig()
-//                .buildRound(downArrow, mContext.getResources().getColor(R.color.red_monza)); //unicode uparrow
-//
-//
-//        v.mImageAgn.setImageDrawable(drawable);
-
-//        check whether to open close or leave a card alone
-//        if (position == expandedPosition) {
-//            v.mExpandArea.setVisibility(View.VISIBLE);
-//        } else {
-//            v.mExpandArea.setVisibility(View.GONE);
-//        }
 
     }
 
@@ -303,60 +247,6 @@ public class LeagueTableAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
     @Override
     public int getItemCount() {
         return leagueTeams.size();
-    }
-
-
-    public class ExpandAnimation extends Animation {
-        private View mAnimatedView;
-        private RelativeLayout.LayoutParams mViewLayoutParams;
-        private int mMarginStart, mMarginEnd;
-        private boolean mIsVisibleAfter = false;
-        private boolean mWasEndedAlready = false;
-
-        /**
-         * Initialize the animation
-         * @param view The layout we want to animate
-         * @param duration The duration of the animation, in ms
-         */
-        public ExpandAnimation(View view, int duration) {
-
-            setDuration(duration);
-            mAnimatedView = view;
-            mViewLayoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
-
-            // decide to show or hide the view
-            mIsVisibleAfter = (view.getVisibility() == View.VISIBLE);
-
-            mMarginStart = mViewLayoutParams.bottomMargin;
-            mMarginEnd = (mMarginStart != 0 ? (0- view.getHeight()) : 0);
-
-            view.setVisibility(View.VISIBLE);
-        }
-
-        @Override
-        protected void applyTransformation(float interpolatedTime, Transformation t) {
-            super.applyTransformation(interpolatedTime, t);
-
-            if (interpolatedTime < 1.0f) {
-
-                // Calculating the new bottom margin, and setting it
-                mViewLayoutParams.bottomMargin = mMarginStart
-                        + (int) ((mMarginEnd - mMarginStart) * interpolatedTime);
-
-                // Invalidating the layout, making us seeing the changes we made
-                mAnimatedView.requestLayout();
-
-                // Making sure we didn't run the ending before (it happens!)
-            } else if (!mWasEndedAlready) {
-                mViewLayoutParams.bottomMargin = mMarginEnd;
-                mAnimatedView.requestLayout();
-
-                if (mIsVisibleAfter) {
-                    mAnimatedView.setVisibility(View.GONE);
-                }
-                mWasEndedAlready = true;
-            }
-        }
     }
 
 }
